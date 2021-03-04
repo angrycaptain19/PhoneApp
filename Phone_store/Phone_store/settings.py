00da3c00b42d20b4d8e3aaeec62cf2b2dedb2d5f
unknown <16520450@gm.uit.edu.vn>
2021-03-04 11:07:02 +0700

    Sửa file setting lần 2

--- Phone_store/Phone_store/settings.py
+++ Phone_store/Phone_store/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '5$gw06)zv51#^q+!5)cn_0!(0d#8m79^ex@90m5$*ykz8irs+r'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://ez-phonestore.herokuapp.com/']
+ALLOWED_HOSTS = ['ez-phonestore.herokuapp.com']
 
 
 # Application definition
